36fc25905a52819786a29e0f5e9c44d4e8914516
“Laura <laureen.pelaez.isaza@gmail.com>
2017-06-28 23:12:00 -0400

    updated jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,24 @@
-
-/*$(document).ready(function () {
-    $("button").click(function (e) {
-        e.preventDefault();
-        var text = $(".text").val();
-        if (text == "") {
-            alert("Debes escribir una tarea");
-        } else {
-            $('#to-do').append('<div><form> class="paragraph>' + text + '</form><p class="center-align>' + text + '</p></div>');
-        }
-
-    });
-    //Delete button click event
-    $('.deleteButton').click(function () {
-        deleteRow($(this));
-    });
-
- * Deletes the grandparent of the delete button
- 
-    function deleteRow(thisButton) {
-        thisButton.parent().parent().remove();
-    }
-});*/
-
+var i = 0; //contador, para poder crear ID dinamicos en la variable html, con un onclick.
 $(document).ready(function () {
 
+    $('#new-task').keydown(onKeyPress);//listener para cuando las teclas esten oprimidas.
 
 });//end dom ready
+
+function onKeyPress(event) {
+    if (event.keyCode == 13) { //Este ENTER key se encontro evento en: https://msdn.microsoft.com/en-us/library/aa243025(v=vs.60).aspx
+        if ($('#new-task').val() != '') {//Si el valor del new task no esta vacìo, continue.
+            var html = '<div class="row item" id="item_' + i + '"><div class="col s10"><input type="checkbox" ><label for="item" onclick="onSelectItem(item_' + i + ')">' + $('#new-task').val() + '</label> </div>'; //Esta variable crea el html con sus clases y pone un onclick que va creando divs dinamicos, llamando a la clase new-task
+            html += '<div class="col s2"><button class="btn waves-effect waves-light green deleteButton" type="button" onclick="OnRemove(item_' + i + ')">Remove</button></div></div>'; //Concateno el valor html creado con el boton 
+            $(".todo-items").append(html);//selecciono el div con la clase y en esta le adiciono el texto html
+            $('#new-task').val(''); //borro contenido
+            i++; //vaya sumando mi variable contador
+        } else {
+            alert('La tarea no puede estar vacía');
+        }
+    }
+}
+function OnRemove(item) { //Quito los elementos creados
+    $(item).remove();
+}
+
